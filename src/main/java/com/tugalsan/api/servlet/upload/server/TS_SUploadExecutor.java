3ce23295e59c00ed5bca2595b32ab14f75f5d54e
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-26 13:41:20 +0300

    Update TS_SUploadExecutor.java

--- src/main/java/com/tugalsan/api/servlet/upload/server/TS_SUploadExecutor.java
+++ src/main/java/com/tugalsan/api/servlet/upload/server/TS_SUploadExecutor.java
@@ -1,13 +1,13 @@
 package com.tugalsan.api.servlet.upload.server;
 
 
-import com.tugalsan.api.callable.client.TGS_CallableType3Void;
+import com.tugalsan.api.callable.client.TGS_CallableType3_Run;
 import java.time.Duration;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-abstract public class TS_SUploadExecutor implements TGS_CallableType3Void<HttpServlet, HttpServletRequest, HttpServletResponse> {
+abstract public class TS_SUploadExecutor implements TGS_CallableType3_Run<HttpServlet, HttpServletRequest, HttpServletResponse> {
 
     public Duration timeout() {
         return Duration.ofMinutes(1);
